6b2301fef5ec89cff3c3e8ebd479a0b84cf7cbc2
sandeepyadav02 <etawah2010@gmail.com>
2018-10-13 22:26:44 +0530

    Implementation for patient address is complete

--- src/main/java/com/demo/patient/controller/PatientController.java
+++ src/main/java/com/demo/patient/controller/PatientController.java
@@ -1,16 +1,24 @@
 package com.demo.patient.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.tools.DocumentationTool.Location;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.demo.patient.model.Patient;
 
-import com.demo.patient.repository.PatientRepository;
-import com.demo.patient.service.PatientService;
+
+
+
 
 @RestController
 @RequestMapping(value = "/api")
@@ -27,8 +35,19 @@ public class PatientController {
 		return patientService.getPatientDetails();
 
 	}
+    
 
-	
+	@RequestMapping(value = "/Patient{id}", method = RequestMethod.DELETE)
+	public void deletepatient(@PathVariable long pid) {
+		PatientRepository.deleteById(pid);
 		
 
+	}
+	@RequestMapping(value = "/Patient", method = RequestMethod.POST)
+	public ResponseEntity<Object> createPatient(@RequestBody Patient patient) {
+		Patient savedPatient = PatientRepository.save(patient);
+        return ResponseEntity.created(location).build();
+
+	}
+    
 }
